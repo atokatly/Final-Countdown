ce34b2a62ed1c524c882750742ec451b92b64e90
Anthony Tokatly <atokatly884@gmail.com>
2017-01-05 12:14:22 -0800

    add user login functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "users#index"
   resources :users
+
+  # Session controllers 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
   # get '/users/new' => 'users#new'
   # post '/users' => 'users#create'
 end
