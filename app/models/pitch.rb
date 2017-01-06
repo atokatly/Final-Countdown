9cbd29ef2980000b4105764c83108bc0365a2876
Ibraheem Abdul-Malik & Katie O'Neill <pair+ibraheem4.katieboundary@devbootcamp.com>
2017-01-05 19:30:42 -0800

    Add thank you logic,

--- app/models/pitch.rb
+++ app/models/pitch.rb
@@ -3,4 +3,13 @@ class Pitch < ApplicationRecord
   has_many :students
   accepts_nested_attributes_for :students
   has_many :preferences
+
+  #student.pitches.length > 1 redirect to "Thank you" page. < 1 redirect to new pitch page
+
+  def pitch_length
+
+  end
+
+
+
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -3,4 +3,6 @@ class Student < ApplicationRecord
   belongs_to :team, optional: true
   alias_attribute :team, :pitch
   has_many :preferences
+
+  validates :pitches, length: {maximum: 2}
 end
